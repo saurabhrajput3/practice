9f6a3fcbcedace06a2825640582a221ba957a9ea
saurabhrajput3 <97332062+saurabhrajput3@users.noreply.github.com>
2023-01-29 13:24:26 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.net.http.HttpResponse;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-String url="https://api.chucknorris.io/jokes/random";
+String url="https://jsonplaceholder.typicode.com/albums";
 
     //Building a request
         HttpRequest request= HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
