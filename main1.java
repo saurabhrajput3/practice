c812c0f02ed575c46d6e06b3c6f45c2e7c591f12
saurabhrajput3 <97332062+saurabhrajput3@users.noreply.github.com>
2023-01-29 13:29:47 +0530

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,23 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String url="https://api.chucknorris.io/jokes/random";
+
+        //Building a request
+        HttpRequest request= HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+
+        //Getting a HttpClient object
+        HttpClient client=HttpClient.newBuilder().build();
+
+        HttpResponse<String> response=client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+
+    }
+}
