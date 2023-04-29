e91e284c168d4536b376832f1ea3566c4928b987
saurabh rajput <97332062+saurabhrajput3@users.noreply.github.com>
2023-04-29 23:06:33 +0530

    Delete Doctor-Patient-Application/Doctor-Patient-Application directory

--- Doctor-Patient-Application/Doctor-Patient-Application/DoctorPatient/Doctor_Patient/src/main/java/com/example/demo/exceptionHandler/NotValid.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exceptionHandler;
-
-public class NotValid extends Throwable{
-    public NotValid(String message){
-        super(message);
-    }
-}
--- Doctor-Patient-Application/Doctor-Patient-Application/DoctorPatient/Doctor_Patient/src/main/java/com/example/demo/repo/IStatusRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.helper.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IStatusRepo extends JpaRepository<Status,Integer> {
-}
